66997e21fd5ba8570dadf00cfa8ab2a32cb09b9c
arslan-khan <muhammad.arslan@arbisoft.com>
2018-08-19 17:52:08 +0500

    sort questions by timestamp

--- src/selectors/questionsSelectors.js
+++ src/selectors/questionsSelectors.js
@@ -1,17 +1,21 @@
 const getAnsweredQuestions = (questions, userId) =>
   questions &&
-  Object.values(questions).filter(
-    question =>
-      question.optionOne.votes.includes(userId) ||
-      question.optionTwo.votes.includes(userId),
-  );
+  Object.values(questions)
+    .filter(
+      question =>
+        question.optionOne.votes.includes(userId) ||
+        question.optionTwo.votes.includes(userId),
+    )
+    .sort(question => question.timestamp);
 
 const getUnansweredQuestions = (questions, userId) =>
   questions &&
-  Object.values(questions).filter(
-    question =>
-      !question.optionOne.votes.includes(userId) &&
-      !question.optionTwo.votes.includes(userId),
-  );
+  Object.values(questions)
+    .filter(
+      question =>
+        !question.optionOne.votes.includes(userId) &&
+        !question.optionTwo.votes.includes(userId),
+    )
+    .sort(question => question.timestamp);
 
 export { getAnsweredQuestions, getUnansweredQuestions };
